e096476e7ce69e39446a1602f2bc6dff4611e240
Certsoftt <91556576+Certsoftt@users.noreply.github.com>
2024-11-25 23:23:30 +0100

    Update App.tsx
    added toast alert

--- App.tsx
+++ App.tsx
@@ -3,6 +3,8 @@ import './Form.css'
 import {Box} from '@mui/material'
 import Theme from './component/MaterilaUI/Dynamic Fields/Theme';
 import Fallback from './component/MaterilaUI/Dynamic Fields/Fallback';
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.min.css'
 
 const LazyDynamicInput = React.lazy(()=>import('./component/MaterilaUI/Dynamic Fields/DynamicInput'))
 
@@ -14,6 +16,7 @@ function App() {
           <LazyDynamicInput/>
         </Theme>
       </React.Suspense>
+      <ToastContainer/>
     </Box>
   );
 }
